625f0977d29bc12c7e1916e4b51d32fe3c8939bf
Lauro Bezerra Soares <lbezerrs@everis.com>
2019-09-27 11:54:44 -0300

    Animações do MenuItem

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { MenuItem } from './menu-item.model';
+// import { trigger } from '@angular/core/src/animation/dsl';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'mt-menu-item',
   templateUrl: './menu-item.component.html',
-  styleUrls: ['./menu-item.component.css']
+  // styleUrls: ['./menu-item.component.css'],
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('300ms ease-in')
+      ])
+    ])
+  ]
 })
+
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready'
 
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter()
@@ -18,7 +33,7 @@ export class MenuItemComponent implements OnInit {
   ngOnInit() {
   }
 
-  emitAddEvent(){
+  emitAddEvent() {
     this.add.emit(this.menuItem)
   }
 
--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Restaurant } from './restaurant.model';
 
-import { trigger, state, style, transition, animate } from '@angular/animations'
+import { trigger, state, style, transition, animate } from '@angular/animations' // criar animação na pagina de restaurantes
 
 @Component({
   selector: 'mt-restaurant',
@@ -11,8 +11,8 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
     trigger('restaurantAppeared', [
      state('ready', style({opacity: 1})),
      transition('void => ready',[
-       style({opacity: 0, transform: 'translate(-30px, -10px'}),
-       animate('500ms 0s ease-in-out')
+       style({opacity: 0, transform: 'translate(-30px, -10px'}), // eixos x e y
+       animate('500ms 0s ease-in-out') // entra animação acelerando e sai animação desacelerando
      ])
     ])
   ]
